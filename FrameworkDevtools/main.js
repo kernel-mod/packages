a40c7184325985d75a917da05007a7b5485972d9
Swishilicous <paige.git@gmail.com>
2023-01-21 17:07:54 -0600

    Fix RDT

--- FrameworkDevtools/main.js
+++ FrameworkDevtools/main.js
@@ -1,32 +1,8 @@
-const { app } = require("electron");
-const devtools = require("electron-devtools-installer");
-const fs = require("fs-extra");
-const path = require("path");
+const { join } = require("path");
+const { session, app } = require("electron");
 
-// const broadcast = require("@kernel/core/broadcast");
-// // const { OriginalBrowserWindow } = require("@kernel/core/BrowserWindowPatcher");
+const devtools = join(__dirname, "react-4.25.0");
 
-module.exports.default = {
-	start() {
-		console.log("Starting FrameworkDevtools");
-		app.on("ready", () => {
-			const enabledDevtools = fs.readJSONSync(
-				path.join(__dirname, "index.json")
-			).options.devtools;
-			for (const devtool of enabledDevtools) {
-				devtools
-					.default(devtools[devtool], {
-						loadExtensionOptions: { allowFileAccess: true },
-						forceDownload: true,
-					})
-					.then((name) => console.log(`Added Extension: ${name}`))
-					.catch((err) =>
-						console.error("Failed to add devtools extensions:", err)
-					);
-			}
-		});
-	},
-	stop() {
-		console.log("Restart the app to unload the devtools.");
-	},
-};
+app.whenReady().then(() => {
+	session.defaultSession.loadExtension(devtools);
+});
